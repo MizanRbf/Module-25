da2decea1ceafebd541a647d97213d7199c814e2
MizanRbf <mizanrbf@gmail.com>
2025-02-22 20:02:25 +0600

    module-25 done

---
+++ js dom.js
@@ -0,0 +1,26 @@
+// const allSections = document.getElementsByTagName('section');
+// for(const section of allSections){
+//   section.style.margin = '10px';
+//   section.style.borderRadius = '10px';
+//   // console.log(allSections);
+// }
+
+// const allTitles = document.getElementsByClassName('section-title');
+// for(const title of allTitles){
+//   title.style.marginLeft = '10px';
+//   // title.setAttribute('')
+//   // console.log(title);
+// }
+// const lastSection = document.getElementById('3rd-section');
+// lastSection.style.backgroundColor = 'yellow';
+// // console.log(lastSection);
+
+// const firstFoodContainer = document.getElementById('first-food-container');
+// firstFoodContainer.setAttribute('style', 'list-style: none');
+
+const UL = document.getElementById('fruits-containers');
+const li = document.createElement('li');
+li.innerText = 'Banana';
+UL.appendChild(li);
+
+
